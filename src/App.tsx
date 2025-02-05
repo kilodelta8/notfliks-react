ef36d3800a50fe84c0bfceb95c4be64d520e7b93
kilodelta8 <jdurham937@gmail.com>
2025-02-05 16:58:49 -0500

    init commiit

---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+import './App.css'
+import searchIcon from './assets/Search.svg'
+import MovieCard from './components/MovieCard'
+
+const API_URL = 'http://www.omdbapi.com?apikey=f638d2f0'
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    searchFliks("Batman");
+  }, []);
+
+
+  const searchFliks = async (title: string) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.search);
+  };
+
+
+  return (
+    <>
+      <div className='app'>
+        <h1>Notfliks</h1>
+
+        <div className='search'>
+          <input
+            value={ searchTerm }
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder='Search for Movies'
+          />
+          <img
+            src={searchIcon}
+            alt='search'
+            onClick={() => searchFliks(searchTerm)}
+          />
+        </div>
+          {movies?.length > 0 ? (
+            <div className='container'>
+              {movies.map((movie) => (
+                <MovieCard movie={movie} />
+              ))}
+            </div>
+          ) : (
+            <div className='empty'>
+              <h2>No movies found</h2>
+            </div>
+          )
+        }
+      </div>
+    </>
+  );
+}
+
+export default App
